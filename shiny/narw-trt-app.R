7fe73dd8c1f436c70f8d98aebf9129c5fd38cc11
seanhardison1 <seanhardison@gmail.com>
2019-04-18 14:02:54 -0400

    app space

---
+++ shiny/narw-trt-app.R
@@ -0,0 +1,34 @@
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "NARW TRT Scenario Planning", titleWidth = 300),
+  dashboardSidebar(    
+    sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(plotOutput("plot1", height = 250)),
+      
+      box(
+        title = "Controls",
+        sliderInput("slider", "Number of observations:", 1, 100, 50)
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
+
+shinyApp(ui, server)
