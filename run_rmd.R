2ce0acd7cd04f387e16b7f22f403d218fe6e2d91
andybeet <22455149+andybeet@users.noreply.github.com>
2019-04-19 15:13:16 -0400

    rmd - update to render call

--- run_rmd.R
+++ run_rmd.R
@@ -1,8 +1,8 @@
 InputSpreadsheetName <- "ExampleRun2.csv"
 OutputDir <- gsub(".csv", "", InputSpreadsheetName)
+OutputPathDir <- here::here("Scenarios",OutputDir)
 
 
 
-
-rmarkdown::render(input = here::here("template.Rmd"), params = list(set_title = InputSpreadsheetName,
+rmarkdown::render(input = here::here("template.Rmd"), output_format="html_document",output_file=paste0(OutputDir,".html"),output_dir=OutputPathDir,params = list(set_title = InputSpreadsheetName,
                                                                     set_path = OutputDir))
