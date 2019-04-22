c67550c78030fd12486062d4932574b013d242a6
andybeet <22455149+andybeet@users.noreply.github.com>
2019-04-22 15:55:30 -0400

    update to read in additional shapefiles

--- R/read_shapefiles.r
+++ R/read_shapefiles.r
@@ -4,21 +4,30 @@ shapefilePath <- here::here("InputShapefiles")
 
 message("Reading in shapefiles ...")
 
-iso100ft <-  rgdal::readOGR(dsn = shapefilePath, layer = '100f_Isobath',verbose = F)
-EastCoastLines <-  rgdal::readOGR(dsn = shapefilePath, layer = 'EastCoastLines',verbose = F)
+OffshoreA <-  rgdal::readOGR(dsn = shapefilePath, layer = 'AtlOffshoreLAFigure2_',verbose = F)
+MassExpansion <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Mass_Bay_Expansion_Only',verbose = F)
+CCBay <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Proposed_Cape_Cod_Bay_Area',verbose = F)
+SouthShoreA <-  rgdal::readOGR(dsn = shapefilePath, layer = 'SouthShoreA',verbose = F)
+SouthShoreB <-  rgdal::readOGR(dsn = shapefilePath, layer = 'SouthShoreB',verbose = F)
+SouthShoreC <-  rgdal::readOGR(dsn = shapefilePath, layer = 'SouthShoreC',verbose = F)
+
+#EastCoastLines <-  rgdal::readOGR(dsn = shapefilePath, layer = 'EastCoastLines',verbose = F)
 GB <-  rgdal::readOGR(dsn = shapefilePath, layer = 'GB_100M_to_600M',verbose = F)
 GOM <-  rgdal::readOGR(dsn = shapefilePath, layer = 'GOM_100M_to_EEZ',verbose = F)
-GSC_Gillnet <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Great_South_Channel_Restricted_Gillnet_Area',verbose = F)
-GSC_Trap <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Great_South_Channel_Restricted_Trap-Pot_Area',verbose = F)
-GSC_Sliver <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Great_South_Channel_Sliver_Restricted_Area',verbose = F)
+#GSC_Gillnet <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Great_South_Channel_Restricted_Gillnet_Area',verbose = F)
+#GSC_Trap <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Great_South_Channel_Restricted_Trap-Pot_Area',verbose = F)
+#GSC_Sliver <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Great_South_Channel_Sliver_Restricted_Area',verbose = F)
 LCMAs <-  rgdal::readOGR(dsn = shapefilePath, layer = 'LCMAs',verbose = F)
 MASS_RA <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Massachusetts_Restricted_Area',verbose = F)
 MASS_RANE <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Massachusetts_Restricted_Area_North_Expansion',verbose = F)
 NEA_NR <-  rgdal::readOGR(dsn = shapefilePath, layer = 'NEA_Nantucket_Rectangle',verbose = F)
 NEA_WGOM <-  rgdal::readOGR(dsn = shapefilePath, layer = 'NEA_WGOM_Area',verbose = F)
-SA_DT <-  rgdal::readOGR(dsn = shapefilePath, layer = 'StatAreas_DecisionTool',verbose = F)
-SA_537 <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Statistical_Area_537',verbose = F)
-
+#SA_DT <-  rgdal::readOGR(dsn = shapefilePath, layer = 'StatAreas_DecisionTool',verbose = F)
+#SA_537 <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Statistical_Area_537',verbose = F)
+StatAreas <-  rgdal::readOGR(dsn = shapefilePath, layer = 'Statistical_Areas',verbose = F)
+areas <- c("464" ,"465" ,"511" ,"511", "512", "512", "513" ,"514" ,"515", "521", "522", "525", "526",
+           "537" ,"538" ,"538", "539", "561" ,"562", "616")
+StatAreas <- raster::subset(StatAreas,Id %in% areas)
 message("Done")
 
 
