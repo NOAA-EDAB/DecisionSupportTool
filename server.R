d4171388b21fcd17c40b10d617f561441ba51851
slarge <largesi@gmail.com>
2019-04-23 15:15:17 -0400

    fix readme input

--- server.R
+++ server.R
@@ -529,6 +529,6 @@ function(input, output, session) {
   })
   
   output$renderedReadme <- renderUI({           
-    includeHTML(rmarkdown::render(input = "readme.md", "html_document"))
+    includeHTML(rmarkdown::render(input = "README.md", "html_document"))
   })
 }
