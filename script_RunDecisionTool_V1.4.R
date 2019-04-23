914ae63b0d8b3301debba8a909326a972ff474cd
seanhardison1 <seanhardison@gmail.com>
2019-04-23 17:46:24 -0400

    shapes

--- script_RunDecisionTool_V1.4.R
+++ script_RunDecisionTool_V1.4.R
@@ -23,7 +23,7 @@ source(file.path(r.dir,"read_shapefiles.R"))
 source(file.path(r.dir,"model-specs.R"))
 # source("function_DecisionSupportTool_V1.2.R")
 source(file.path(r.dir,"run_decisiontool.R"))
-
+load("TrapMap_V0.1.Rdata")
 
 ## set working directory
 HD=here::here()
