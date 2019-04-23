e5a0443ddd88acd10bdc2966c8f49d7ff747eda1
slarge <largesi@gmail.com>
2019-04-23 08:38:04 -0400

    add demo for tables
    * sandbox as proof of concept

--- tab_app_example.R
+++ tab_app_example.R
@@ -6,52 +6,71 @@
 #
 
 library(shiny)
-
-ui = shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins
-  # sidebarLayout(
-  #   sidebarPanel(
-  #     sliderInput("bins",
-  #                 "Number of bins:",
-  #                 min = 1,
-  #                 max = 50,
-  #                 value = 30)
-  #   ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      tabsetPanel(
-        tabPanel(
-          "2 columns example",
-          fluidRow(
-            column(width = 6,
-                   h2("Relative Risk"),
-                   DT::dataTableOutput('RelativeRisk')),
-            column(width = 6,
-                   h2("Vertical Lines"),
-                   DT::dataTableOutput('VerticalLines'))
-            ),
-          fluidRow(
-            column(width = 6,
-                   h2("Traps Fished"),
-                   DT::dataTableOutput('TrapsFished')),
-            column(width = 6,
-                   h2("Trawls"),
-                   DT::dataTableOutput('Trawls')
-            ))
-        )
-        
-      )
+ui <- dashboardPage(
+  dashboardHeader(title = "ALW TRT Scenario Planning", titleWidth = 300),
+  dashboardSidebar(    
+    sidebarMenu(
+      menuItem("View Tables", tabName = "view_tables", icon = icon("th"))
     )
-))
+  ),
+  dashboardBody(    
+    
+    tabItem(tabName = "view_tables",
+            fluidRow(
+              box(width = 5,
+                  h4("Relative Risk"),
+                  DT::dataTableOutput('RelativeRisk')),
+              box(width = 5,
+                  h4("Vertical Lines"),
+                  DT::dataTableOutput('VerticalLines'))
+            ),
+            fluidRow(
+              box(width = 5,
+                  h4("Traps Fished"),
+                  DT::dataTableOutput('TrapsFished')),
+              box(width = 5,
+                  h4("Trawls"),
+                  DT::dataTableOutput('Trawls')
+              )
+            )
+    )
+  )
+)
+# ui = shinyUI(fluidPage(
+#   
+#   # Application title
+#   titlePanel("TEST"),
+# 
+#     
+#     # Show a plot of the generated distribution
+#     mainPanel(
+#       tabsetPanel(
+#         tabPanel(
+#           "2 columns example",
+#           fluidRow(
+#             column(width = 6,
+#                    h2("Relative Risk"),
+#                    DT::dataTableOutput('RelativeRisk')),
+#             column(width = 6,
+#                    h2("Vertical Lines"),
+#                    DT::dataTableOutput('VerticalLines'))
+#             ),
+#           fluidRow(
+#             column(width = 6,
+#                    h2("Traps Fished"),
+#                    DT::dataTableOutput('TrapsFished')),
+#             column(width = 6,
+#                    h2("Trawls"),
+#                    DT::dataTableOutput('Trawls')
+#             ))
+#         )
+#         
+#       )
+#     )
+# ))
 
 library(shiny)
 library(DT)
-datatable(iris)
 
 server = shinyServer(function(input, output) {
   
@@ -67,7 +86,7 @@ server = shinyServer(function(input, output) {
   # })
   dat <- read.csv("Scenarios/ExampleRun/ExampleRun_OutputData.csv", stringsAsFactors = FALSE)
 
-  dat <- dat %>% 
+  dat <- dat %>%
     dplyr::mutate(Default = round(Default, 0),
            Scenario = round(Scenario, 0),
            Reduction = paste0(round(Reduction, 2) * 100, "%"))
@@ -78,21 +97,51 @@ server = shinyServer(function(input, output) {
   
   output$RelativeRisk = DT::renderDataTable(X$RelativeRisk,
                                            rownames = FALSE,
-                                           options = list(pageLength = 13, dom = 't'))
-  
+                                           options = list(pageLength = 13, 
+                                                          dom = 't',
+                                                          autoWidth = TRUE,
+                                                          columnDefs = list(list(
+                                                            className = 'dt-right', targets = c(0,3)
+                                                          ))
+                                           ))
+
   output$Trawls = DT::renderDataTable(X$Trawls,
                                       rownames = FALSE,
-                                      options = list(pageLength = 13, dom = 't'))
-  
+                                      options = list(pageLength = 13, 
+                                                     dom = 't',
+                                                     autoWidth = TRUE,
+                                                     columnDefs = list(list(
+                                                       className = 'dt-right', targets = c(0,3)
+                                                     ))
+                                      ))
   
   output$TrapsFished = DT::renderDataTable(X$TrapsFished,
                                            rownames = FALSE,
-                                           options = list(pageLength = 13, dom = 't'))
+                                           options = list(pageLength = 13, 
+                                                          dom = 't',
+                                                          autoWidth = TRUE,
+                                                          columnDefs = list(list(
+                                                            className = 'dt-right', targets = c(0,3)
+                                                          ))
+                                           ))
   
-  output$VerticalLines = DT::renderDataTable(X$VerticalLines,
-                                      rownames = FALSE,
-                                      options = list(pageLength = 13, dom = 't'))
-  
+  output$VerticalLines = DT::renderDT({  #Tables loaded from the proper Scenario folder
+    # dat <- read.csv("Scenarios/ExampleRun/ExampleRun_OutputData.csv", stringsAsFactors = FALSE) %>% 
+    dat <- read.csv(matched_tables(), stringsAsFactors = FALSE) %>%
+      dplyr::mutate(Default = round(Default, 0),
+                    Scenario = round(Scenario, 0),
+                    Reduction = paste0(round(Reduction, 2) * 100, "%")) %>% 
+      dplyr::filter(Variable == "RelativeRisk") %>% 
+      dplyr::select(-Variable) %>% 
+      DT::datatable(., rownames = FALSE,
+                    options = list(pageLength = 13, 
+                                   dom = 't',
+                                   autoWidth = TRUE,
+                                   columnDefs = list(list(
+                                     className = 'dt-right', targets = c(0,3)
+                                   ))
+                    ))
+  })
 })
 
 
